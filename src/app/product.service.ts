adf2ad57fb0afa624e3fd52a794cf77d3b23b0e7
Adekanye Oluwayomi <52767095+kanyeunited@users.noreply.github.com>
2019-11-02 17:58:16 +0100

    Using Interfaces to describe data

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { Album } from './album';
+import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
-import 'rxjs/Observable';
 
 @Injectable()
 export class ProductService {
